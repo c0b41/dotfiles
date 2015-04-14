3155a3ab0c4ef6d92355c7ca52f554356f28a55e
ayhankuru <ayhankuru@yandex.com.tr>
2015-04-14 15:29:26 +0300

    fix

--- elementary-os-install.sh
+++ elementary-os-install.sh
@@ -1,1 +1,3 @@
-sudo apt-get install libmagickwand-dev imagemagick
+sudo apt-get update
+sudo apt-get -y install git-core
+sudo apt-get -y install libmagickwand-dev imagemagick
--- iojs.nightly.sh
+++ iojs.nightly.sh
@@ -6,13 +6,14 @@
 #  sudo ./install-iojs-nightly.sh next
 
 type=nightly
-if [ "X$1" == "Xnext" ]; then
+if [ "X$1" == "Xnext" ]
   type=next-nightly
+then
+  type=release
 fi
-
 latest=$(curl -sL https://iojs.org/download/${type}/index.tab | head -2 | tail -1 | awk '{ print $1 }')
-url="https://iojs.org/download/${type}/$latest/iojs-${latest}-linux-x64.tar.xz"
+url="https://iojs.org/download/${type}/$latest/iojs-${latest}-linux-x64.tar.gz"
 
 echo "Downloading ${url}..."
 
-curl -sL $url | xzcat | tar -vx --strip-components=1 -C /usr/local/
+curl -sL $url | tar -vx --strip-components=1 -C /usr/local/
