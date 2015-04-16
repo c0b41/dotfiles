582d7e3262e86c636a0af4efcf31ca7617c92311
ayhankuru <ayhankuru@yandex.com.tr>
2015-04-16 17:37:09 +0300

    test

--- .funcs/mp3name.sh
+++ .funcs/mp3name.sh
@@ -1,7 +1,8 @@
 function mp3name
 {
-find $HOME/Müzik -type f -name '*.mp3' | while read i; do
-	name="$(echo $i | ytclear )"
-   echo $name
+find $HOME/Müzik/$1 -type f -name '*.mp3' | while read i; do
+	name="$(echo $i | ytclear )"`
+    echo $name
+    #todo
 done
 }
