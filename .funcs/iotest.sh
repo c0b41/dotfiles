fdedfa9ee5d6575652b629e08ede04ef1612b469
ayhankuru <ayhankuru@yandex.com.tr>
2015-06-21 23:07:48 +0300

    fix

--- .funcs/iotest.sh
+++ .funcs/iotest.sh
@@ -6,8 +6,8 @@ data="$(cat <<-EOF
 	  - iojs-v2.3.0
 	EOF
 	)" 
- datas="$(cat package.json | jq '.scripts.test="mocha --harmony ./test/test.js --reporter spec --timeout 15000 --require mocha-clean"')"
+ #datas="$(cat package.json | jq '.scripts.test="mocha --harmony ./test/test.js --reporter spec --timeout 15000 --require mocha-clean"')"
  command echo "$data" > .travis.yml 
- command echo "$datas" > package.json
+ #command echo "$datas" > package.json
  echo -e "${blue} Travis ci dosyası oluşturuldu :)  ${reset}"
 }
