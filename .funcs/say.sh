10db09ceda5b7556d57c5e7a409166a252d254f7
ayhankuru <ayhankuru@yandex.com.tr>
2015-07-09 23:28:30 +0300

    fix

--- .funcs/say.sh
+++ .funcs/say.sh
@@ -1,6 +1,6 @@
 function say(){
 
-	p=$(echo $1 |sed 's/ /+/g');
-	wget -U Mozilla -q -O - "$@" translate.google.com/translate_tts?tl=tr\&q=$p|mpg123 - &> /dev/null  
+	p=$(echo $1 |sed 's/ /%20/g');
+	wget -U Mozilla -q -O - "$@" translate.google.com/translate_tts?tl=tr\&q=$p|mpg123 -  
 
 }
