abb224ca87295dd62dc93aac6a995eba9b12bd40
ayhankuru <ayhankuru@yandex.com.tr>
2015-08-15 01:24:01 +0300

    added lossy

--- .funcs/joinimage.sh
+++ .funcs/joinimage.sh
@@ -2,3 +2,8 @@ function joinimage
 {
    command montage -mode concatenate -tile 1x $@
 }
+
+function joinimage2
+{
+   command montage -mode concatenate -tile 2x $@
+}
--- elementary-os-install.sh
+++ elementary-os-install.sh
@@ -3,3 +3,6 @@ sudo apt-get -y install git-core
 sudo apt-get -y install libmagickwand-dev imagemagick
 sudo apt-get -y install festival
 sudo apt-get -y install mpg123
+curl -OJL https://github.com/pornel/giflossy/releases/download/lossy%2F1.82.1/gifsicle-1.82.1-lossy.zip
+unzip gifsicle-1.82.1-lossy.zip -d gifsicle
+sudo mv gifsicle/linux/gifsicle-debian6 /usr/local/bin/gifsicle
