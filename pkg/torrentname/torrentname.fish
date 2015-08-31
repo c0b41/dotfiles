e490ee71ade3067a4f79e37925cdce6cad1bf3ec
ayhankuru <ayhankuru@yandex.com.tr>
2015-08-31 23:33:10 +0300

    fish shell package

---
+++ pkg/torrentname/torrentname.fish
@@ -0,0 +1,19 @@
+# SYNOPSIS
+#   torrentname [name]
+#
+# USAGE
+#   name
+#
+
+function torrentname
+  if not available torrent-name jq
+    echo "📂  Please install 'npm install -g torrrent-name && jq ' first!"
+  else    
+  	set data  (eval torrent-name $argv)
+  	set title (echo $data | jq -r '.title' | sed -e "s/\b\(.\)/\u\1/g" )
+  	set season (echo $data | jq -r '.season' )
+  	set episode (echo $data | jq -r '.episode' )
+  	set new "$title  S0$seasonxE0$episode"
+  	echo $new
+  end
+end
