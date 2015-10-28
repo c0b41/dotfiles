501b21286e4a2b07e59420ce9dc7e4ada8c42c6b
ayhankuru <ayhankuru@yandex.com.tr>
2015-10-28 13:11:54 +0300

    iojs line replace nodejs

--- themes/fscoiety/fish_prompt.fish
+++ themes/fscoiety/fish_prompt.fish
@@ -2,8 +2,8 @@ function fish_prompt
   set -l emoji    "👽"
   set -l nick     $nickname
   set -l code     $status
-  set -l iojsv    "asd"
-  set -l gov	  (eval go version | awk '{ print substr( $0, 14 ,6) }')
+  set -l nodejsv  (eval node --version)
+  set -l gov	    (eval go version | awk '{ print substr( $0, 14 ,6) }')
   set -l cyan     (set_color -o cyan)
   set -l yellow   (set_color -o yellow)
   set -l red      (set_color -o red)
@@ -14,7 +14,7 @@ function fish_prompt
 
   echo -n -e (begin
     test $code = 0
-      and echo "$purple ~ $nick $normal [$yellow iojs $normal] $yellow ⚡ $normal [$yellow go $gov $normal] \n"
+      and echo "$purple ~ $nick $normal [$yellow nodejs $nodejsv $normal] $yellow ⚡ $normal [$yellow go $gov $normal] \n"
   end) "$blue"(prompt_pwd) (begin
     echo "$yellow git:("$normal(git_branch_name)"$yellow)"
     git_is_touched; and echo "$yellow⇡⇡"
