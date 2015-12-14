6737e1f22d55f7b8cc6a7c27ab5f568a89f8979b
ayhankuru <ayhankuru@yandex.com.tr>
2015-12-14 11:53:09 +0200

    stuff

--- .funcs/alias.sh
+++ .funcs/alias.sh
@@ -2,11 +2,9 @@ alias ..='cd ..'
 alias ....='cd ../..'
 alias .....='cd ../../..'
 alias ......='cd ../../../..'
-alias today='garden $nickname' 
 alias so='cp $HOME/.dotfiles/.bashrc $HOME/.bashrc  && cp source $HOME/.dotfiles/.gitconfig $HOME && source $HOME/.bashrc'
 alias ioversion='iojs --version'
-gversion=$(goversion) 
+gversion=$(goversion)
 alias wifi-sifre='wifi-password'
 alias wifi-name='wifi-name'
 alias imgur='imgur-uploader'
-
