9d2922f8488bd281fbafdc283bd5c82c073ff4e8
ayhankuru <ayhankuru@yandex.com.tr>
2015-12-31 17:28:57 +0200

    submerge package added

---
+++ pkg/submerge/submerge.fish
@@ -0,0 +1,17 @@
+# SYNOPSIS
+#   submerge [options]
+#
+# USAGE
+#   Options
+#   Src videofile
+#   srt subtitlefile
+#   ouput video
+#   submerge mr.robot.1x01.mkv sub.srt output.mkv
+
+function submerge
+  if not available mencoder
+    echo "📂  Please install mencoder first!"
+  else
+    mencoder $argv[1] -sub $argv[2] -subcp utf8 -subfont-text-scale 3 -subfont-outline 1 -ovc xvid -oac mp3lame -xvidencopts pass=1 -o $argv[2]
+  end
+end
