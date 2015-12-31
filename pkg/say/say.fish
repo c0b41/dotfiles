d0e14066e2ea11ba5b279ee27f5939f862d05b19
ayhankuru <ayhankuru@yandex.com.tr>
2015-12-31 20:27:51 +0200

    fix

--- pkg/say/say.fish
+++ pkg/say/say.fish
@@ -9,9 +9,8 @@ function say
   if not available mpg123
     echo "📂  Please install mpg123 first!"
   else
-    iconv -f Windows-1254 -t UTF-8 $argv[1] > $argv[2]
-    set p eval(echo $argv | sed 's/ /+/g');
+    set p (echo $argv | sed 's/ /+/g');
 	  set URL "http://translate.google.com/translate_tts?tl=tr&q=$p";
-	  wget -U Mozilla -q  -O - "$@" "$URL" | mpg123 - &> /dev/null
+	  curl -A "Mozilla" "$URL" | mpg123 - >> /dev/null
   end
 end
