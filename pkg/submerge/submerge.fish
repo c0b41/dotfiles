d97f65ad359ab1f7f9e69734efae634716f8c4e9
ayhankuru <ayhankuru@yandex.com.tr>
2016-01-06 21:46:15 +0200

    fix argument

--- pkg/submerge/submerge.fish
+++ pkg/submerge/submerge.fish
@@ -12,6 +12,6 @@ function submerge
   if not available mencoder
     echo "📂  Please install mencoder first!"
   else
-    mencoder $argv[1] -sub $argv[2] -subcp utf8 -subfont-text-scale 3 -subfont-outline 1 -ovc xvid -oac mp3lame -xvidencopts pass=1 -o $argv[2]
+    mencoder $argv[1] -sub $argv[2] -subcp utf8 -subfont-text-scale 3 -subfont-outline 1 -ovc xvid -oac mp3lame -xvidencopts pass=1 -o $argv[3]
   end
 end
